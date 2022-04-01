734918174f45317ef2d62d9fa6a0e96082dad876
Vincent <bsvtgc@gmail.com>
2022-04-01 11:46:32 -0400

    configure trap base address and a bit of cleanup

--- build/build.sh
+++ build/build.sh
@@ -19,33 +19,56 @@ fi
 
 if [[ "cleanse" == "$1" ]]; then
     rm *.elf
+    rm *.o
     exit 1
 fi
 
 BOARD=$1
 
-build_executable ()
+set_asembler ()
 {
-    printf "Build for BOARD=$1"
-    printf "Right now this just builds the start.S\n"
-
-    SRC_FILES=../boards/hifive1-revb/start.S
-
 # GAS (GNU Assembler)
     GNU_AS=riscv64-unknown-elf-as
     GNU_AS_FLAGS="-march=rv32imac -mno-arch-attr" 
     GNU_AS_OUTPUT_EXT=.o 
+}
 
+set_linker ()
+{
 # GNU Linker
     GNU_LD=riscv64-unknown-elf-ld
     GNU_LD_SCRIPT=$BOARD.lscript
     GNU_LD_FLAGS=-belf32-littleriscv
+}
+
+set_compiler ()
+{
+# GNU Compiler
+    GNU_CC=riscv64-unknown-elf-cc
+#    GNU_CC_FLAGS=
+}
+
+build_executable ()
+{
+    printf "Build for BOARD=${BOARD}\n"
+    printf "Right now this just builds the basic *.S files \n"
+
+    BOARD_DIR=../boards/hifive1-revb
+    SRC_FILES=$(ls ${BOARD_DIR})
 
 # Executable 
     EXECUTABLE=FINAL_LOAD.elf
 
-    $GNU_AS  $GNU_AS_FLAGS $SRC_FILES -o $SRC_FILES.$GNU_AS_OUTPUT_EXT
-    $GNU_LD -T../tools/$GNU_LD_SCRIPT  $GNU_LD_FLAGS $SRC_FILES.$GNU_AS_OUTPUT_EXT -o $EXECUTABLE
+    for file in ${SRC_FILES[@]}; do
+        file_trimmed=$( echo "$file" | cut -f 1 -d '.')
+        $GNU_AS  ${GNU_AS_FLAGS} ${BOARD_DIR}/${file} -o ${file_trimmed}.o
+    done
+
+    OBJ_FILES=$( ls *.o )
+    #printf "Object files $OBJ_FILES\n"
+    $GNU_LD -T../tools/$GNU_LD_SCRIPT  $GNU_LD_FLAGS $OBJ_FILES -o $EXECUTABLE
 }
 
+set_asembler;
+set_linker;
 build_executable;
